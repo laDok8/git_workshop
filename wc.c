4672203dc47e2611ee40c073cebfa15cc0940b35
laDok8 <strasak.d7@gmail.com>
2019-10-30 11:59:14 +0100

    Added -w

--- wc.c
+++ wc.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 
 
 int main(int argc, char **argv){
@@ -9,10 +10,15 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
-	if(strcmp("-c", argv[1]) == 0){
+	if(strcmp("-c", argv[1]) == 0 || strcmp("-w", argv[1]) == 0){
 		int c,i=0;
 		while((c = fgetc(f)) != EOF){
-			i++;
+			if(strcmp("-w", argv[1]) == 0 &&  isspace(c)){
+					i++;
+			}
+			else if(strcmp("-c", argv[1]) == 0){
+				i++;
+			}
 		}
 		printf("%d\n", i);
 	}
